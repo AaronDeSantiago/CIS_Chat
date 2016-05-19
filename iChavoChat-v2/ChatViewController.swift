14de5267141915d04b382f721b16e7543bbac206
Aaron De Santiago <desantiago.aaron@gmail.com>
2016-05-19 10:09:27 -0600

    first commit

---
+++ iChavoChat-v2/ChatViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ChatViewController.swift
+//  iChavoChat-v2
+//
+//  Created by Aaron De Santiago on 4/30/16.
+//  Copyright © 2016 Aaron De Santiago. All rights reserved.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
